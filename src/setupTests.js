831366c46b947c7397cf49102de0897a08639a6f
Lu Nguyen <lu@r-digital.tech>
2022-10-21 15:07:23 +0700

    Fix

--- src/setupTests.js
+++ src/setupTests.js
@@ -8,3 +8,10 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+const localStorageMock = {
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+  clear: jest.fn(),
+};
+global.localStorage = localStorageMock;
