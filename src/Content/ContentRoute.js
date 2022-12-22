3467797f41457d147ffcdbeb504893cbe1da97f6
Toan Pham <toan@redweb.dk>
2022-12-22 14:57:12 +0700

    update

--- src/Content/ContentRoute.js
+++ src/Content/ContentRoute.js
@@ -54,11 +54,11 @@ class ContentRoute extends BaseRoute {
    * @param contentId
    */
   deleteContentRequest = (contentId) => {
-    const list = JSON.parse(contentId);
+    const contentIds = JSON.parse(contentId);
     return AesirxApiInstance().delete(
       this.createRequestURL({
         option: 'content',
-        'ids[]': contentId,
+        'ids[]': contentIds,
       })
     );
   };
