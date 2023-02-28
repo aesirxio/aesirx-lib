47fe644203224b7a5206243d65fad18e2d8bd976
huy <huytran.dev.web@gmail.com>
2023-02-28 10:04:26 +0700

    add login app

--- src/__test__/globalSetup.js
+++ src/__test__/globalSetup.js
@@ -3,7 +3,7 @@ import { AXIOS_CONFIGS, AUTHORIZATION_KEY } from '../Constant/Constant';
 
 module.exports = async function () {
   const service = new AesirxAuthenticationApiService();
-  const data = await service.login(AXIOS_CONFIGS.USERNAME, AXIOS_CONFIGS.PASSWORD);
+  const data = await service.loginTest(AXIOS_CONFIGS.USERNAME, AXIOS_CONFIGS.PASSWORD);
 
   process.env.accessToken = data[AUTHORIZATION_KEY.ACCESS_TOKEN] ?? '';
   process.env.refreshToken = data[AUTHORIZATION_KEY.REFRESH_TOKEN] ?? '';
