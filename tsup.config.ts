adeba192772107f3b35922d24d2c4df96a5292c1
Lu Nguyen <lu@r-digital.tech>
2023-03-29 17:03:01 +0700

    lint

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,15 +4,12 @@ const env = process.env.NODE_ENV;
 
 export const tsup: Options = {
   legacyOutput: true,
-  splitting: true,
   clean: true, // clean up the dist folder
   dts: true, // generate dts files
   format: ['cjs', 'esm'], // generate cjs and esm files
   minify: env === 'production',
   bundle: false,
-  skipNodeModulesBundle: true,
-  entryPoints: ['src/index.ts'],
   watch: env === 'development',
   outDir: 'dist',
-  entry: ['src/**/*.ts'], //include all files under src
+  entry: ['src/index.ts', 'src/**/*.ts'],
 };
