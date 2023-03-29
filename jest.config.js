ec8cfb55173fe80877d9f92e1f53ce236b6315a7
Lu Nguyen <lu@r-digital.tech>
2023-03-29 14:17:53 +0700

    change test

--- jest.config.js
+++ jest.config.js
@@ -4,8 +4,8 @@ require('dotenv').config();
 module.exports = {
   roots: ['<rootDir>/src'],
   testTimeout: 20000,
-  setupFilesAfterEnv: ['<rootDir>/__test__/setupTests.js'],
-  globalSetup: '<rootDir>/__test__/globalSetup.js',
+  setupFilesAfterEnv: ['<rootDir>/test/setupTests.js'],
+  globalSetup: '<rootDir>/test/globalSetup.js',
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
   globals: {
     'ts-jest': {
