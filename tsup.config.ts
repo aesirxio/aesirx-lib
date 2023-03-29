82353cd28a7e92c9623d4ae73331424110763e35
Lu Nguyen <lu@r-digital.tech>
2023-03-29 16:24:24 +0700

    Fix module

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,12 +3,13 @@ import type { Options } from 'tsup';
 const env = process.env.NODE_ENV;
 
 export const tsup: Options = {
+  legacyOutput: true,
   splitting: true,
   clean: true, // clean up the dist folder
   dts: true, // generate dts files
   format: ['cjs', 'esm'], // generate cjs and esm files
   minify: env === 'production',
-  bundle: env === 'production',
+  bundle: false,
   skipNodeModulesBundle: true,
   entryPoints: ['src/index.ts'],
   watch: env === 'development',
