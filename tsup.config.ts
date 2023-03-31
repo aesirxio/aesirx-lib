ad7a44eec86aacde815df3b79ca1c87aedcca212
Lu Nguyen <lu@r-digital.tech>
2023-03-31 14:12:04 +0700

    only esm

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,13 +3,17 @@ import type { Options } from 'tsup';
 const env = process.env.NODE_ENV;
 
 export const tsup: Options = {
-  legacyOutput: true,
   clean: true, // clean up the dist folder
   dts: true, // generate dts files
-  format: ['cjs', 'esm'], // generate cjs and esm files
+  format: ['esm'], // generate cjs and esm files
   minify: env === 'production',
   bundle: false,
   watch: env === 'development',
   outDir: 'dist',
   entry: ['src/index.ts', 'src/**/*.ts'],
+  outExtension() {
+    return {
+      js: `.js`,
+    };
+  },
 };
