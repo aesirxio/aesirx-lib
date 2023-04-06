1dd72bc95f055d6993190fd083bf3d4649d2b83b
Lu Nguyen <lu@r-digital.tech>
2023-04-06 16:19:05 +0700

    exclude test

--- tsup.config.ts
+++ tsup.config.ts
@@ -5,11 +5,11 @@ const env = process.env.NODE_ENV;
 export const tsup: Options = {
   clean: true, // clean up the dist folder
   dts: true, // generate dts files
-  format: ['esm'], // generate cjs and esm files
+  format: ['esm'],
   minify: env === 'production',
   watch: env === 'development',
   outDir: 'dist',
-  entry: ['src/index.ts', 'src/**/*.ts'],
+  entry: ['src/index.ts', 'src/**/*.ts', '!src/**/*.test.{ts,tsx}'],
   target: 'es2020',
   outExtension() {
     return {
