04d590e0b56bd10af39277a60c5446774b76aa66
Viet <viet@r-digital.tech>
2023-05-31 15:58:54 +0700

    Update Model

--- src/Constant/OrganizationContent.ts
+++ src/Constant/OrganizationContent.ts
@@ -1,5 +1,17 @@
 const ORGANISATION_MEMBER_FIELD: any = {
   ID: 'id',
+  MEMBER_NAME: 'member_name',
+  ROLE_ID: 'role_id',
+  MEMBER_EMAIL: 'member_email',
+  MEMBER_ROLE: 'member_role',
+  ORGANISATION: 'organisation',
+  ORGANISATION_ID: 'organisation_id',
+  MEMBER_STATE: 'member_state',
+  CREATED_USER_NAME: 'created_user_name',
+  MODIFIED_USER_NAME: 'modified_user_name',
+  CREATED_TIME: 'created_time',
+  PUBLISH_UP: 'publish_up',
+  MODIFIED_TIME: 'modified_time',
 };
 
 const ORGANISATION_ROLE_FIELD: any = {
--- src/Organization/Member/Model.ts
+++ src/Organization/Member/Model.ts
@@ -20,11 +20,35 @@ class OrganizationMemberModel extends BaseModel {
 
 class OrganizationMemberItemModel extends BaseItemModel {
   id: any = null;
+  member_name: any = null;
+  role_id: any = null;
+  member_email: any = null;
+  member_role: any = null;
+  organisation: any = null;
+  organisation_id: any = null;
+  member_state: any = null;
+  created_user_name: any = null;
+  modified_user_name: any = null;
+  created_time: any = null;
+  publish_up: any = null;
+  modified_time: any = null;
 
   constructor(entity: any) {
     super(entity);
     if (entity) {
       this.id = entity[ORGANISATION_MEMBER_FIELD.ID] ?? '';
+      this.member_name = entity[ORGANISATION_MEMBER_FIELD.MEMBER_NAME] ?? '';
+      this.role_id = entity[ORGANISATION_MEMBER_FIELD.ROLE_ID] ?? '';
+      this.member_email = entity[ORGANISATION_MEMBER_FIELD.MEMBER_EMAIL] ?? '';
+      this.member_role = entity[ORGANISATION_MEMBER_FIELD.MEMBER_ROLE] ?? '';
+      this.organisation = entity[ORGANISATION_MEMBER_FIELD.ORGANISATION] ?? '';
+      this.organisation_id = entity[ORGANISATION_MEMBER_FIELD.ORGANISATION_ID] ?? '';
+      this.member_state = entity[ORGANISATION_MEMBER_FIELD.MEMBER_STATE] ?? '';
+      this.created_user_name = entity[ORGANISATION_MEMBER_FIELD.CREATED_USER_NAME] ?? '';
+      this.modified_user_name = entity[ORGANISATION_MEMBER_FIELD.MODIFIED_USER_NAME] ?? '';
+      this.created_time = entity[ORGANISATION_MEMBER_FIELD.CREATED_TIME] ?? '';
+      this.publish_up = entity[ORGANISATION_MEMBER_FIELD.PUBLISH_UP] ?? '';
+      this.modified_time = entity[ORGANISATION_MEMBER_FIELD.MODIFIED_TIME] ?? '';
     }
   }
 
@@ -32,6 +56,18 @@ class OrganizationMemberItemModel extends BaseItemModel {
     return {
       ...this.baseToJSON(),
       [ORGANISATION_MEMBER_FIELD.ID]: this.id,
+      [ORGANISATION_MEMBER_FIELD.MEMBER_NAME]: this.member_name,
+      [ORGANISATION_MEMBER_FIELD.ROLE_ID]: this.role_id,
+      [ORGANISATION_MEMBER_FIELD.MEMBER_EMAIL]: this.member_email,
+      [ORGANISATION_MEMBER_FIELD.MEMBER_ROLE]: this.member_role,
+      [ORGANISATION_MEMBER_FIELD.ORGANISATION]: this.organisation,
+      [ORGANISATION_MEMBER_FIELD.ORGANISATION_ID]: this.organisation_id,
+      [ORGANISATION_MEMBER_FIELD.MEMBER_STATE]: this.member_state,
+      [ORGANISATION_MEMBER_FIELD.CREATED_USER_NAME]: this.created_user_name,
+      [ORGANISATION_MEMBER_FIELD.MODIFIED_USER_NAME]: this.modified_user_name,
+      [ORGANISATION_MEMBER_FIELD.CREATED_TIME]: this.created_time,
+      [ORGANISATION_MEMBER_FIELD.PUBLISH_UP]: this.publish_up,
+      [ORGANISATION_MEMBER_FIELD.MODIFIED_TIME]: this.modified_time,
     };
   };
 }
